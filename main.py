e5941906d1992a0b8bb4fbbc84ad2310feb83963
Rinoapouz <Danielzilkee@gmail.com>
2024-06-08 20:20:19 +0200

    change #1

--- main.py
+++ main.py
@@ -3,29 +3,33 @@ from disnake.ext import commands
 from commands.commands_user import commands_user
 from commands.commands_admin import commands_admin
 
-# Definiere eine Liste der erlaubten Server-IDs
+# Liste der erlaubten Server-IDs
 allowed_server_ids = [1248558780195799041, 808816437363343380]
 
+# Prefix wurde bestimmt (Standard /)
 bot = commands.Bot(
     command_prefix="ari",
     intents=disnake.Intents.all()
 )
 
 
-# Zeigt in der CMD mit welchem BOT angemeldet ist
+# Überprüfe die Server-IDs und leavt unerwünschte Server
 @bot.event
 async def on_ready():
     print(f"Bot is running | {bot.user}")
-    # Überprüfe die Server-IDs und verlasse unerwünschte Server
+    # Überprüfe die Server-IDs und leavt unerwünschte Server
     for guild in bot.guilds:
         if guild.id not in allowed_server_ids:
             await guild.leave()
             print(f"Left server [{guild.name}] [ID: {guild.id}] | Server Owner: [{guild.owner}]")
 
 
-# Überprüfe, ob der Bot einem Server beitritt
+# Überprüft, ob der Bot einem unbekannten Server beitritt, wenn ja = verlassen
 @bot.event
 async def on_guild_join(guild):
+    if guild.id not in allowed_server_ids:
+        await guild.leave()
+        print(f"Left server [{guild.name}] [ID: {guild.id}] | Server Owner: [{guild.owner}]")
     if guild.id not in allowed_server_ids:
         await guild.leave()
         print(f"Left server [{guild.name}] [ID: {guild.id}] | Server Owner: [{guild.owner}]")
@@ -35,4 +39,4 @@ async def on_guild_join(guild):
 commands_admin(bot)
 commands_user(bot)
 
-bot.run("MTE3NTAwNTgyODc5Nzk2ODQwNQ.Gmo5Ba.YTtyY-lXY9lBGnjTIXmu-ZbNmn2UuDXeSaow0A")
+bot.run("MTE3NTAwNTgyODc5Nzk2ODQwNQ.GTXkVh.34N-dTa3pbTzZd__qStYTYHhPvPZ7xJQNBQpy8")
