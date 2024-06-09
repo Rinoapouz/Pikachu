43cd37c57d622edea9b66b4f2d71e7287e04769f
Rinoapouz <Danielzilkee@gmail.com>
2024-06-09 17:33:34 +0200

    Edited

--- commands/admin/ban.py
+++ commands/admin/ban.py
@@ -2,9 +2,22 @@ from permissions.permissions import *
 
 
 def ban(bot):
-    @bot.command()
     @has_administrator_role()
-    async def ban(ctx, member: disnake.Member, *, reason: str = "Kein Grund angegeben") -> None:
+    @bot.slash_command(
+        name="ban",
+        description="Ban a user")
+    async def slashban(ctx, member: disnake.Member, *, reason: str = "Kein Grund angegeben") -> None:
+        if member.id == ctx.author.id:
+            await ctx.send("Du kannst dich nicht selbst bannen, tut mir leid! :)")
+            return
+        await ctx.guild.ban(member, reason=reason)
+        await ctx.send(f"{member} wurde für {reason} gebannt.")
+
+    @has_administrator_role()
+    @bot.command(
+        name="ban",
+        description="Ban a user",)
+    async def ariban(ctx, member: disnake.Member, *, reason: str = "Kein Grund angegeben") -> None:
         if member.id == ctx.author.id:
             await ctx.send("Du kannst dich nicht selbst bannen, tut mir leid! :)")
             return
