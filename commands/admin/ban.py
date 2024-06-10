5dff51bcf60d6d6d0984b8657a92477909e239b6
rinoapouz <Danielzilkee@gmail.com>
2024-06-10 11:59:08 +0200

    Edited

--- commands/admin/ban.py
+++ commands/admin/ban.py
@@ -7,7 +7,22 @@ def ban(bot):
     @has_administrator_role()
     @bot.slash_command(
         name="ban",
-        description="Ban a user")
+        description="Ban a user",
+        options=[
+            disnake.Option(
+                name="member",
+                description="The member to ban",
+                type=disnake.OptionType.user,
+                required=True
+            ),
+            disnake.Option(
+                name="reason",
+                description="The reason for the ban",
+                type=disnake.OptionType.string,
+                required=False,
+            )
+        ]
+    )
     async def slashban(ctx, member: disnake.Member, *, reason: str = "Kein Grund angegeben") -> None:
         if member.id == ctx.author.id:
             await ctx.send("You can't ban yourself, idiot! :)")
@@ -22,13 +37,28 @@ def ban(bot):
 
         embed.add_field(name="User", value=f"{member}", inline=True)
         embed.add_field(name="Reason", value=f"{reason}", inline=True)
-        embed.add_field(name="length", value=f"permanent", inline=True)
+        embed.add_field(name="Length", value=f"permanent", inline=True)
         await ctx.send(embed=embed)
 
     @has_administrator_role()
     @bot.command(
         name="ban",
-        description="Ban a user", )
+        description="Ban a user",
+        options=[
+            disnake.Option(
+                name="member",
+                description="The member to ban",
+                type=disnake.OptionType.user,
+                required=True
+            ),
+            disnake.Option(
+                name="reason",
+                description="The reason for the ban",
+                type=disnake.OptionType.string,
+                required=False,
+            )
+        ]
+    )
     async def ariban(ctx, member: disnake.Member, *, reason: str = "Kein Grund angegeben") -> None:
         if member.id == ctx.author.id:
             await ctx.send("You can't ban yourself, idiot! :)")
